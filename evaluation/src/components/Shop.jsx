035ab43149b32e586256c0e5202bcd84184323ca
Mansur Dewan <mansurdewan26@gmail.com>
2021-11-08 16:15:09 +0530

    order item

---
+++ evaluation/src/components/Shop.jsx
@@ -0,0 +1,108 @@
+import { useState } from "react";
+import "./shop.css";
+function Shop() {
+  const [data, setData] = useState({
+    title: "",
+    out: false
+  });
+  const [current, setCurrent] = useState([]);
+  const [order, setOrder] = useState([]);
+  const [list, setList] = useState(true);
+  const handleChange = (e) => {
+    const { name, value, checked, type } = e.target;
+    setData({ ...data, [name]: type === "checkbox" ? checked : value });
+  };
+
+  const handleCurrrent = () => {
+    setCurrent([...current, data]);
+  };
+  const handleOrder = () => {
+    setOrder([...order, data]);
+  };
+
+  const handleList = () => {
+    setList(!list);
+  };
+
+  const handleMove = (e) => {
+    let index;
+    for (let i = 0; i < order.length; i++) {
+      if (e == order[i]) {
+        index = i;
+      }
+    }
+    // let v = order.splice(index, 1);
+    setCurrent([...current, order.splice(index, 1)[0]]);
+  };
+
+  return (
+    <div>
+      <div className="input-section">
+        <input
+          className="input"
+          type="text"
+          name="list"
+          placeholder="Enter the item"
+          onChange={handleChange}
+        />
+        <button className="current" onClick={handleCurrrent}>
+          Add to Current List
+        </button>
+        <button className="order" onClick={handleOrder}>
+          Add to Order list
+        </button>
+      </div>
+      <div className="button">
+        <button className="buttonStyle" onClick={handleList}>
+          {list ? "Show order list" : "show Current list"}
+        </button>
+      </div>
+      <div className="show-data">
+        {list ? (
+          <div>
+            <h1>Current item</h1>
+            <hr />
+            {current.map((e) => (
+              <div>
+                {e.Checkbox ? (
+                  <div className="out">
+                    <h2>out of stock</h2>
+                    <p>{e.list}</p>
+                  </div>
+                ) : (
+                  <div>
+                    <p>{e.list}</p>
+                    <span>out of Stock</span>
+                    <input
+                      type="checkbox"
+                      name="Checkbox"
+                      onChange={handleChange}
+                    />
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div>
+            <h1>Order item</h1>
+            <hr />
+            {order.map((e) => (
+              <div>
+                <p>{e.list}</p>
+                <button
+                  onClick={(e) => {
+                    handleMove(e);
+                  }}
+                >
+                  Move
+                </button>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+export { Shop };
